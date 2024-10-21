3303f6843a691c94b63f4263d8f605538c5954f0
Rémi C. lanxsatura <t4d483@gmail.com>
2024-10-20 18:02:24 -0600

    lftp scripts

---
+++ lftp-tls-configure.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#Author:  Rémi Chauvin
+#Purpose: configure `~/.lftprc` for automatic TLS connections
+#         towards FTPES (explicit SSL/TLS) servers
+#         using a self-signed certificate
+
+
+#check if lftp is installed
+$(which lftp) >/dev/null 2>&1 || echo "lftp is not installed" && exit 1
+
+#check if ~/.lftp/certs folder is present
+[ -d ~/.lftp/certs ] || mkdir -p ~/.lftp/certs
+
+#create a backup of .lftprc if it already exists
+if [ -f ~/.lftprc ]; then
+    cp ~/.lftprc ~/.lftprc.backup.$(date +"%F-at-%R:%S")
+else
+    touch ~/.lftprc
+fi
+
+#static list of recommended options for secure TLS
+LIST=(
+        'set ssl:check-hostname no'
+        'set ftp:ssl-auth TLS'
+        'set ftp:ssl-force true'
+        'set ftp:ssl-protect-list yes'
+        'set ftp:ssl-protect-data yes'
+        'set ftp:ssl-protect-fxp yes'
+        'set ssl:verify-certificate no'
+        'set ftps:initial-prot P'
+    )
+
+#verify the presence of each option in ~/.lftprc
+#if not present, append to file
+for opt in "${LIST[@]}"; do
+    if [[ ! "$(grep -e "${opt}" ~/.lftprc)" ]]; then
+        echo "${opt}" >> ~/.lftprc && echo "Added: ${opt}"
+    fi
+done
+
+#END
